e7598d1660f3a196d17733ae236c54ef17465541
Jean-Michel Alturk <jean-michel.alturk@polymtl.ca>
2023-12-13 01:40:50 -0500

    added benchmarking code to workers, granted benchmarking permission to worker nodes

--- terraform/manager_data.sh
+++ terraform/manager_data.sh
@@ -112,6 +112,9 @@ mysql -u root -e "USE sakila; SELECT COUNT(*) FROM film;"
 mysql -u root -e "USE sakila; SELECT COUNT(*) FROM film_text;"
 
 mysql -u root -e "GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'ip-172-31-19-0.ec2.internal' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'ip-172-31-19-1.ec2.internal' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'ip-172-31-19-2.ec2.internal' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'ip-172-31-19-3.ec2.internal' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 # read write test
 echo "Read-Write Test"
